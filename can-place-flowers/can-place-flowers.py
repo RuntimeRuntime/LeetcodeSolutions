9633f745b5d577fc61ee6fd8ab0935eab3429c54
RuntimeRuntime <86265021+RuntimeRuntime@users.noreply.github.com>
2021-06-22 19:04:43 +0100

    Time: 164 ms, (74.62%) Space: 14.6 MB (64.32% )
    Updated solution to remove messy bits of logic such as try catch
    Also faster and uses less space

--- can-place-flowers/can-place-flowers.py
+++ can-place-flowers/can-place-flowers.py
@@ -6,23 +6,23 @@ class Solution:
         # return 1 if there are no flowers to plant or 0 if the flower bed is empty
         if not list: return 0
         elif n == 0: return 1
-        prev = None
-        nxt = None
-        #For each slot in the flowerbed
+        prev = 0
+        nxt = 0
+        
+        #pad back of bed with a 0
+        bed = flowerbed + [0]
+        
+        #For each slot in bed
         for i in range(0, len(flowerbed)):
-            # Try to set next if available
-            try: nxt = flowerbed[i+1]
-            except: nxt = None
-            curr = flowerbed[i]
-            # if previous and next slot are both empty/dont exist
-            if (prev == None or prev == 0) and (nxt == None or nxt == 0 ) and curr == 0:
+            nxt = bed[i+1]
+            # if previous and next slot are both empty
+            if prev == 0 and nxt == 0 and bed[i] == 0:
                 # Count down the number of flowers left to plant
                 n -= 1
                 # Plant the flower in the slot
-                flowerbed[i], curr = 1,1
-                
+                bed[i] = 1
             # Set Prev to current
-            prev = curr
+            prev = bed[i]
             if n == 0: break
         if n <= 0:
             return 1
